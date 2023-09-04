80af9ce7f0941eace607b6fb07fee8139a7bf5d3
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-09-04 13:22:09 -0300

    Pagina Faqs funcionando ok

--- services/faqs/getFaqs.tsx
+++ services/faqs/getFaqs.tsx
@@ -1,6 +1,6 @@
 
 export const getFaqs = async () => {
-    const response = await fetch(`https://dh-marvel.vercel.app/api/faqs`)
+    const response = await fetch(`https://ctd-esp-fe3-final-swart.vercel.app/api/faqs`)
     const faqs = await response.json()
     return faqs;
 }
