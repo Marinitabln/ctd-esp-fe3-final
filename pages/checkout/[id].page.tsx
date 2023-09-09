794617f4bc5c38af0f189a9744979166f53e6886
Marina Victoria Blanco <marivic_05@hotmail.com>
2023-09-09 17:06:07 -0300

    Avances

--- pages/checkout/[id].page.tsx
+++ pages/checkout/[id].page.tsx
@@ -3,17 +3,12 @@ import Box from '@mui/material/Box'
 import Paper from '@mui/material/Paper'
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button'
+import StepperCheckout from 'dh-marvel/components/checkout/StepperCheckout';
 
 
 const CheckoutPage = () => {
 
 	const [step, setStep] = useState(1)
-
-	const {handleSubmit} = useFormContext();
-
-	const onSubmit = (data: any) => {
-		
-	};
 
 	const nextStep = () => {
 		setStep(step + 1)
@@ -24,30 +19,10 @@ const CheckoutPage = () => {
 	}
 
 	return (
-		<Box sx={{maxWidth: "500px"}}>
-			<Paper
-				elevation={1}
-				sx={{p: "32px", display: "flex", flexDirection: "column", gap: 3}}
-			>
-				<Typography variant="h4" align="center">
-					Datos personales
-				</Typography>
-				<form onSubmit={handleSubmit(onSubmit)}>
-
-					{step === 1 && ''}
-
-					{step === 2 && ''}
-      
-					<Box>
-						{step > 1 && <Button onClick={prevStep}>Anterior</Button>}
-						{step < 2 && <Button onClick={nextStep}>Siguiente</Button>}
-						{step === 2 && <Button type="submit">Enviar</Button>}
-					</Box>
-				</form>
-			</Paper>
-		</Box>
+		<StepperCheckout />
+		
 	);
-};
+
 }
 
 
